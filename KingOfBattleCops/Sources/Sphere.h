8e62015424532fbd171e0eeef471a62c3c8ceb21
wighs33 <genji@kakao.com>
2023-04-01 17:09:24 +0900

    Sphere클래스 생성

---
+++ KingOfBattleCops/Sources/Sphere.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Shape.h"
+
+class Sphere : public Shape
+{
+public:
+    Sphere() {
+        path_ = "Resources/sphere.obj";
+    }
+
+    void Init_And_Render(int model) {
+        glm::mat4 m_transform = glm::mat4(1.0f);
+        m_transform = glm::translate(m_transform, glm::vec3(0.0, 0.0, 0.0));
+        m_transform = glm::scale(m_transform, glm::vec3(1.5, 1.5, 1.5));
+
+        glUniformMatrix4fv(model, 1, GL_FALSE, glm::value_ptr(m_transform));
+
+        glBindVertexArray(VAO_ID);
+        glDrawArrays(GL_TRIANGLES, 0, NUMOF_VERTICES);
+    }
+
+    static const int NUMOF_VERTICES = 2880;
+};
